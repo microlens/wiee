eaf62d700bfa0a6e34944025a4637e5d860eecc0
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-09-04 23:34:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Only You | Photos by Long Nhat Nguyen
+
+Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Using the Gatsby Theme [`@lekoarts/gatsby-theme-emilia`](https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emilia).
+
+## Installation
+
+```sh
+npm install --legacy-peer-deps
+npm run develop
+```
+
+The site is now running at `http://localhost:8000`!
---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Only You`,
+    siteTitleAlt: `Only You | Long Nhat Nguyen`,
+    siteHeadline: `Only You | Long Nhat Nguyen`,
+    siteUrl: `https://only-you.netlify.app`,
+    siteDescription: "Photos by Long Nhat Nguyen",
+    siteLanguage: `en`,
+    author: `@torn4dom4n`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {
+        name: `Only You`,
+        socialMedia: [{}],
+        location: `Vietnam`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Only You | Long Nhat Nguyen`,
+        short_name: `Only You`,
+        description: `Photos by Long Nhat Nguyen`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#3182ce`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ],
+}
